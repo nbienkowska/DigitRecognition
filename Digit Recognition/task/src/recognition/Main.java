e57b97b88637de9f266dade921edd8022e504bff
nbienkowska <nati.bienkowska@gmail.com>
2021-09-20 19:52:03 +0200

    Stage 1: Neuron Creator

--- Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/task/src/recognition/Main.java
@@ -3,6 +3,43 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        ArrayList<Integer> list = processInput();
+        findDigit(list);
+
+    }
+
+    public static ArrayList<Integer> processInput() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int x = 0; x < 3; x++) {
+            String input = sc.next();
+            System.out.println(input);
+            for (int i = 0; i < 3; i++) {
+                if (input.charAt(i) == 'X') {
+                    list.add(1);
+                } else {
+                    list.add(0);
+                }
+            }
+        }
+
+        return list;
+    }
+
+    public static void findDigit(ArrayList<Integer> list) {
+        final int[] weights = {2, 1, 2, 4, -4, 4, 2, -1, 2};
+        final int b = -5;
+        int a10 = 0;
+        for (int i = 0; i < 9; i++) {
+            a10 += list.get(i) * weights[i];
+        }
+
+        a10 += b;
+
+        if(a10 > 0) {
+            System.out.println("This number is 0");
+        } else {
+            System.out.println("This number is 1");
+        }
     }
 }
